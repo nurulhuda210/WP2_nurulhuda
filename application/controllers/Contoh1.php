dd68cc009d3b92677913c2f4e6419a8d7cf1c8cc
nurulhuda210 <tyuinchang@gmail.com>
2021-09-28 06:47:58 +0700

    TUGAS WP2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php 
+class Contoh1 extends CI_Controller {
+    public function index()
+    {
+        echo"<h1> Halo Perkenalkan </h1>";
+        echo"Nama       : saya Nurul Huda <br>
+        Saya tinggal di : Pulomas,Jakarta Timur <br>
+        
+        Saat ini saya berkuliah di BSI <br>
+        Hobi saya adalah Bermain Catur"; 
+    }
+}
+
+?>

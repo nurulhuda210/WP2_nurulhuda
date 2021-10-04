1880e26ac520083de002edf1551ce67c03449950
nurulhuda210 <tyuinchang@gmail.com>
2021-10-04 20:38:06 +0700

    TUGAS WP2

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Nurul </li>
+        <li>Nama Belakang : Huda</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Pulomas 1, Jakarta Timur</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>DKI Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Game</li>
+        <li>Catur</li>
+    </ul>
+</section>
